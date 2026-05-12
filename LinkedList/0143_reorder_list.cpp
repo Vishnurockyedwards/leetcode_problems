5667121f2ed6e9513ee0ce41ce6150f2efbc73c4
vishnurockyedwarsd <VISHNU.23FE10CAI00271@muj.manipal.edu>
2026-05-12 12:17:41 +0530

    reorder list

---
+++ LinkedList/0143_reorder_list.cpp
@@ -0,0 +1,59 @@
+/*
+problem : reorder list
+*/
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+// Definition for singly-linked list
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (!head || !head->next) return;
+
+        // Step 1: Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Step 2: Reverse second half
+        ListNode* prev = nullptr;
+        ListNode* curr = slow->next;
+        slow->next = nullptr; // Split the list into two halves
+
+        while (curr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        // Step 3: Merge two halves
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while (second) {
+            ListNode* tmp1 = first->next;
+            ListNode* tmp2 = second->next;
+
+            first->next = second;
+            second->next = tmp1;
+
+            first = tmp1;
+            second = tmp2;
+        }
+    }
+};
