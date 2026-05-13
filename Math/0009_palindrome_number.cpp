50a82a25b61f3e88aaa092d19371d02e18affe36
vishnurockyedwarsd <VISHNU.23FE10CAI00271@muj.manipal.edu>
2026-05-13 20:55:11 +0530

    reverse integer && pelindrome numbers

---
+++ Math/0009_palindrome_number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers and numbers ending with 0 (but not 0 itself) are not palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int reversedHalf = 0;
+        while (x > reversedHalf) {
+            int digit = x % 10;
+            reversedHalf = reversedHalf * 10 + digit;
+            x /= 10;
+        }
+        if((x==reversedHalf)||(x==reversedHalf/10) ){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
