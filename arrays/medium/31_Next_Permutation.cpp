2f5843b9d7bc1bfdde7b5d625c3c1a216cc9d213
vishnurockyedwarsd <VISHNU.23FE10CAI00271@muj.manipal.edu>
2026-05-17 12:08:43 +0530

    search in rotated sorted array

---
+++ arrays/medium/31_Next_Permutation.cpp
@@ -0,0 +1,27 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+
+        // Step 1: Find first decreasing element from the right
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: If found, find just larger element to swap
+        if (i >= 0) {
+            int j = n - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+
+        // Step 3: Reverse the elements from i+1 to end
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
